24c912d56c7bb61b263719c73e1d5ad780a6dccf
Mohit Gullani <mohitgullani9@gmail.com>
2024-07-07 18:07:58 -0500

    Problem 'Max Consecutive Ones' added in Linked_List_And_Arrays

---
+++ Linked_List_And_Arrays/Max_Consecutive_Ones.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Max_Consecutive_Ones {
+    
+    /*
+        Optimal Solution
+        TC -> O(N)
+        SC -> O(1)
+    */
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0, maxOnes = 0, n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 1){
+                count++;
+            }else{
+                count = 0;
+            }
+            maxOnes = Math.max(maxOnes, count);
+        }
+        return maxOnes;
+    }
+
+    public static void main(String[] args) {
+        // Test Cases
+        int[] nums1 = {1, 1, 0, 1, 1, 1};
+        int[] nums2 = {1, 0, 1, 1, 0, 1};
+
+        System.out.println("There are " + findMaxConsecutiveOnes(nums1) + " consecutive ones in the array -> " + Arrays.toString(nums1));
+        System.out.println("There are " + findMaxConsecutiveOnes(nums2) + " consecutive ones in the array -> " + Arrays.toString(nums2));
+    }   
+}
