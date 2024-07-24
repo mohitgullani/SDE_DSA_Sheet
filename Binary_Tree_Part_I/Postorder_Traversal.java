eb457a8917a51c85da13382e75884925f2e32ba7
Mohit Gullani <mohitgullani9@gmail.com>
2024-07-23 22:44:04 -0500

    Problem 'Postorder Traversal of Binary Tree' added in Binary_Tree_Part_I

---
+++ Binary_Tree_Part_I/Postorder_Traversal.java
@@ -0,0 +1,81 @@
+/*
+    Problem Name: Postorder Traversal of Binary Tree
+    Problem Link: https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Postorder_Traversal {
+    /*
+        Left : Right : Root
+        TC -> O(N) {As we are visiting each node exactly once}
+        SC -> O(N){In the worst case due to recursion stack} otherwise it's O(logN){Recursion Stack Space}
+    */
+    public static void postorderTraversalHelper(TreeNode root, List<Integer> list){
+        if(root == null)    return;
+
+        postorderTraversalHelper(root.left, list);
+        postorderTraversalHelper(root.right, list);
+        list.add(root.val);
+    }
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorderTraversalHelper(root, list);
+        return list;
+    }
+
+    public static class TreeNode{
+        int val;
+        TreeNode left, right;
+        TreeNode(){}
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test Cases
+        Integer[] nums1 = {1, null, 2, 3};
+        Integer[] nums2 = {};
+        Integer[] nums3 = {1};
+
+        TreeNode root1 = insertLevelOrder(nums1);
+        TreeNode root2 = insertLevelOrder(nums2);
+        TreeNode root3 = insertLevelOrder(nums3);
+
+        System.out.println(postorderTraversal(root1));
+        System.out.println(postorderTraversal(root2));
+        System.out.println(postorderTraversal(root3));
+    }
+
+    public static TreeNode insertLevelOrder(Integer[] arr) {
+        if (arr.length == 0 || arr[0] == null) return null;
+
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+        while (i < arr.length) {
+            TreeNode current = queue.poll();
+
+            if (arr[i] != null) {
+                current.left = new TreeNode(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            if (i < arr.length && arr[i] != null) {
+                current.right = new TreeNode(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+}
